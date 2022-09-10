2ad3126fadcc42b1498f1fcf5802ea0643fd304e
Rafael <rafaelquintanilha@hotmail.com>
2022-09-09 21:56:47 -0300

    sounds for each pkm

---
+++ pokedex/src/components/imports.js
@@ -0,0 +1,307 @@
+import p1 from '../sounds/poke1.mp3'
+import p2 from '../sounds/poke2.mp3'
+import p3 from '../sounds/poke3.mp3'
+import p4 from '../sounds/poke4.mp3'
+import p5 from '../sounds/poke5.mp3'
+import p6 from '../sounds/poke6.mp3'
+import p7 from '../sounds/poke7.mp3'
+import p8 from '../sounds/poke8.mp3'
+import p9 from '../sounds/poke9.mp3'
+import p10 from '../sounds/poke10.mp3'
+import p11 from '../sounds/poke11.mp3'
+import p12 from '../sounds/poke12.mp3'
+import p13 from '../sounds/poke13.mp3'
+import p14 from '../sounds/poke14.mp3'
+import p15 from '../sounds/poke15.mp3'
+import p16 from '../sounds/poke16.mp3'
+import p17 from '../sounds/poke17.mp3'
+import p18 from '../sounds/poke18.mp3'
+import p19 from '../sounds/poke19.mp3'
+import p20 from '../sounds/poke20.mp3'
+import p21 from '../sounds/poke21.mp3'
+import p22 from '../sounds/poke22.mp3'
+import p23 from '../sounds/poke23.mp3'
+import p24 from '../sounds/poke24.mp3'
+import p25 from '../sounds/poke25.mp3'
+import p26 from '../sounds/poke26.mp3'
+import p27 from '../sounds/poke27.mp3'
+import p28 from '../sounds/poke28.mp3'
+import p29 from '../sounds/poke29.mp3'
+import p30 from '../sounds/poke30.mp3'
+import p31 from '../sounds/poke31.mp3'
+import p32 from '../sounds/poke32.mp3'
+import p33 from '../sounds/poke33.mp3'
+import p34 from '../sounds/poke34.mp3'
+import p35 from '../sounds/poke35.mp3'
+import p36 from '../sounds/poke36.mp3'
+import p37 from '../sounds/poke37.mp3'
+import p38 from '../sounds/poke38.mp3'
+import p39 from '../sounds/poke39.mp3'
+import p40 from '../sounds/poke40.mp3'
+import p41 from '../sounds/poke41.mp3'
+import p42 from '../sounds/poke42.mp3'
+import p43 from '../sounds/poke43.mp3'
+import p44 from '../sounds/poke44.mp3'
+import p45 from '../sounds/poke45.mp3'
+import p46 from '../sounds/poke46.mp3'
+import p47 from '../sounds/poke47.mp3'
+import p48 from '../sounds/poke48.mp3'
+import p49 from '../sounds/poke49.mp3'
+import p50 from '../sounds/poke50.mp3'
+import p51 from '../sounds/poke51.mp3'
+import p52 from '../sounds/poke52.mp3'
+import p53 from '../sounds/poke53.mp3'
+import p54 from '../sounds/poke54.mp3'
+import p55 from '../sounds/poke55.mp3'
+import p56 from '../sounds/poke56.mp3'
+import p57 from '../sounds/poke57.mp3'
+import p58 from '../sounds/poke58.mp3'
+import p59 from '../sounds/poke59.mp3'
+import p60 from '../sounds/poke60.mp3'
+import p61 from '../sounds/poke61.mp3'
+import p62 from '../sounds/poke62.mp3'
+import p63 from '../sounds/poke63.mp3'
+import p64 from '../sounds/poke64.mp3'
+import p65 from '../sounds/poke65.mp3'
+import p66 from '../sounds/poke66.mp3'
+import p67 from '../sounds/poke67.mp3'
+import p68 from '../sounds/poke68.mp3'
+import p69 from '../sounds/poke69.mp3'
+import p70 from '../sounds/poke70.mp3'
+import p71 from '../sounds/poke71.mp3'
+import p72 from '../sounds/poke72.mp3'
+import p73 from '../sounds/poke73.mp3'
+import p74 from '../sounds/poke74.mp3'
+import p75 from '../sounds/poke75.mp3'
+import p76 from '../sounds/poke76.mp3'
+import p77 from '../sounds/poke77.mp3'
+import p78 from '../sounds/poke78.mp3'
+import p79 from '../sounds/poke79.mp3'
+import p80 from '../sounds/poke80.mp3'
+import p81 from '../sounds/poke81.mp3'
+import p82 from '../sounds/poke82.mp3'
+import p83 from '../sounds/poke83.mp3'
+import p84 from '../sounds/poke84.mp3'
+import p85 from '../sounds/poke85.mp3'
+import p86 from '../sounds/poke86.mp3'
+import p87 from '../sounds/poke87.mp3'
+import p88 from '../sounds/poke88.mp3'
+import p89 from '../sounds/poke89.mp3'
+import p90 from '../sounds/poke90.mp3'
+import p91 from '../sounds/poke91.mp3'
+import p92 from '../sounds/poke92.mp3'
+import p93 from '../sounds/poke93.mp3'
+import p94 from '../sounds/poke94.mp3'
+import p95 from '../sounds/poke95.mp3'
+import p96 from '../sounds/poke96.mp3'
+import p97 from '../sounds/poke97.mp3'
+import p98 from '../sounds/poke98.mp3'
+import p99 from '../sounds/poke99.mp3'
+import p100 from '../sounds/poke100.mp3'
+import p101 from '../sounds/poke101.mp3'
+import p102 from '../sounds/poke102.mp3'
+import p103 from '../sounds/poke103.mp3'
+import p104 from '../sounds/poke104.mp3'
+import p105 from '../sounds/poke105.mp3'
+import p106 from '../sounds/poke106.mp3'
+import p107 from '../sounds/poke107.mp3'
+import p108 from '../sounds/poke108.mp3'
+import p109 from '../sounds/poke109.mp3'
+import p110 from '../sounds/poke110.mp3'
+import p111 from '../sounds/poke111.mp3'
+import p112 from '../sounds/poke112.mp3'
+import p113 from '../sounds/poke113.mp3'
+import p114 from '../sounds/poke114.mp3'
+import p115 from '../sounds/poke115.mp3'
+import p116 from '../sounds/poke116.mp3'
+import p117 from '../sounds/poke117.mp3'
+import p118 from '../sounds/poke118.mp3'
+import p119 from '../sounds/poke119.mp3'
+import p120 from '../sounds/poke120.mp3'
+import p121 from '../sounds/poke121.mp3'
+import p122 from '../sounds/poke122.mp3'
+import p123 from '../sounds/poke123.mp3'
+import p124 from '../sounds/poke124.mp3'
+import p125 from '../sounds/poke125.mp3'
+import p126 from '../sounds/poke126.mp3'
+import p127 from '../sounds/poke127.mp3'
+import p128 from '../sounds/poke128.mp3'
+import p129 from '../sounds/poke129.mp3'
+import p130 from '../sounds/poke130.mp3'
+import p131 from '../sounds/poke131.mp3'
+import p132 from '../sounds/poke132.mp3'
+import p133 from '../sounds/poke133.mp3'
+import p134 from '../sounds/poke134.mp3'
+import p135 from '../sounds/poke135.mp3'
+import p136 from '../sounds/poke136.mp3'
+import p137 from '../sounds/poke137.mp3'
+import p138 from '../sounds/poke138.mp3'
+import p139 from '../sounds/poke139.mp3'
+import p140 from '../sounds/poke140.mp3'
+import p141 from '../sounds/poke141.mp3'
+import p142 from '../sounds/poke142.mp3'
+import p143 from '../sounds/poke143.mp3'
+import p144 from '../sounds/poke144.mp3'
+import p145 from '../sounds/poke145.mp3'
+import p146 from '../sounds/poke146.mp3'
+import p147 from '../sounds/poke147.mp3'
+import p148 from '../sounds/poke148.mp3'
+import p149 from '../sounds/poke149.mp3'
+import p150 from '../sounds/poke150.mp3'
+import p151 from '../sounds/poke151.mp3'
+
+
+export {
+    p1,
+    p2,
+    p3,
+    p4,
+    p5,
+    p6,
+    p7,
+    p8,
+    p9,
+    p10,
+    p11,
+    p12,
+    p13,
+    p14,
+    p15,
+    p16,
+    p17,
+    p18,
+    p19,
+    p20,
+    p21,
+    p22,
+    p23,
+    p24,
+    p25,
+    p26,
+    p27,
+    p28,
+    p29,
+    p30,
+    p31,
+    p32,
+    p33,
+    p34,
+    p35,
+    p36,
+    p37,
+    p38,
+    p39,
+    p40,
+    p41,
+    p42,
+    p43,
+    p44,
+    p45,
+    p46,
+    p47,
+    p48,
+    p49,
+    p50,
+    p51,
+    p52,
+    p53,
+    p54,
+    p55,
+    p56,
+    p57,
+    p58,
+    p59,
+    p60,
+    p61,
+    p62,
+    p63,
+    p64,
+    p65,
+    p66,
+    p67,
+    p68,
+    p69,
+    p70,
+    p71,
+    p72,
+    p73,
+    p74,
+    p75,
+    p76,
+    p77,
+    p78,
+    p79,
+    p80,
+    p81,
+    p82,
+    p83,
+    p84,
+    p85,
+    p86,
+    p87,
+    p88,
+    p89,
+    p90,
+    p91,
+    p92,
+    p93,
+    p94,
+    p95,
+    p96,
+    p97,
+    p98,
+    p99,
+    p100,
+    p101,
+    p102,
+    p103,
+    p104,
+    p105,
+    p106,
+    p107,
+    p108,
+    p109,
+    p110,
+    p111,
+    p112,
+    p113,
+    p114,
+    p115,
+    p116,
+    p117,
+    p118,
+    p119,
+    p120,
+    p121,
+    p122,
+    p123,
+    p124,
+    p125,
+    p126,
+    p127,
+    p128,
+    p129,
+    p130,
+    p131,
+    p132,
+    p133,
+    p134,
+    p135,
+    p136,
+    p137,
+    p138,
+    p139,
+    p140,
+    p141,
+    p142,
+    p143,
+    p144,
+    p145,
+    p146,
+    p147,
+    p148,
+    p149,
+    p150,
+    p151
+    
+}
--- pokedex/src/global/GlobalState.js
+++ pokedex/src/global/GlobalState.js
@@ -9,7 +9,8 @@ import gotcha from '../sounds/Gotcha.mp3'
 import pcOn from '../sounds/pcon.mp3'
 import pcOff from '../sounds/pcoff.mp3'
 import run from '../sounds/run.mp3'
-import apress from '../sounds/apress.mp3'
+import { p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50, p51, p52, p53, p54, p55, p56, p57, p58, p59, p60, p61, p62,p63, p64, p65, p66, p67, p68, p69, p70, p71, p72, p73, p74, p75, p76, p77, p78, p79, p80, p81, p82, p83, p84, p85, p86, p87, p88, p89, p90, p91, p92, p93, p94, p95, p96, p97, p98, p99, p100, p101, p102, p103, p104, p105, p106, p107, p108, p109, p110, p111, p112, p113, p114, p115, p116, p117, p118, p119, p120, p121, p122, p123, p124, p125, p126, p127, p128, p129, p130, p131, p132, p133, p134, p135, p136, p137, p138, p139, p140, p141, p142, p143, p144, p145, p146, p147, p148, p149, p150, p151 } from '../components/imports'
+
 
 function GlobalState(props) {
 
@@ -93,8 +94,465 @@ function GlobalState(props) {
         new Audio(run).play();
     }
 
-    const playAPress = () => {
-        new Audio(apress).play();
+    const playPokeSound = (id) => {
+        let pokesound
+        switch (id) {
+            case 1:
+                pokesound = p1;
+                break;
+            case 2:
+                pokesound = p2;
+                break;
+            case 3:
+                pokesound = p3;
+                break;
+            case 4:
+                pokesound = p4;
+                break;
+            case 5:
+                pokesound = p5;
+                break;
+            case 6:
+                pokesound = p6;
+                break;
+            case 7:
+                pokesound = p7;
+                break;
+            case 8:
+                pokesound = p8;
+                break;
+            case 9:
+                pokesound = p9;
+                break;
+            case 10:
+                pokesound = p10;
+                break;
+            case 11:
+                pokesound = p11;
+                break;
+            case 12:
+                pokesound = p12;
+                break;
+            case 13:
+                pokesound = p13;
+                break;
+            case 14:
+                pokesound = p14;
+                break;
+            case 15:
+                pokesound = p15;
+                break;
+            case 16:
+                pokesound = p16;
+                break;
+            case 17:
+                pokesound = p17;
+                break;
+            case 18:
+                pokesound = p18;
+                break;
+            case 19:
+                pokesound = p19;
+                break;
+            case 20:
+                pokesound = p20;
+                break;
+            case 21:
+                pokesound = p21;
+                break;
+            case 22:
+                pokesound = p22;
+                break;
+            case 23:
+                pokesound = p23;
+                break;
+            case 24:
+                pokesound = p24;
+                break;
+            case 25:
+                pokesound = p25;
+                break;
+            case 26:
+                pokesound = p26;
+                break;
+            case 27:
+                pokesound = p27;
+                break;
+            case 28:
+                pokesound = p28;
+                break;
+            case 29:
+                pokesound = p29;
+                break;
+            case 30:
+                pokesound = p30;
+                break;
+            case 31:
+                pokesound = p31;
+                break;
+            case 32:
+                pokesound = p32;
+                break;
+            case 33:
+                pokesound = p33;
+                break;
+            case 34:
+                pokesound = p34;
+                break;
+            case 35:
+                pokesound = p35;
+                break;
+            case 36:
+                pokesound = p36;
+                break;
+            case 37:
+                pokesound = p37;
+                break;
+            case 38:
+                pokesound = p38;
+                break;
+            case 39:
+                pokesound = p39;
+                break;
+            case 40:
+                pokesound = p40;
+                break;
+            case 41:
+                pokesound = p41;
+                break;
+            case 42:
+                pokesound = p42;
+                break;
+            case 43:
+                pokesound = p43;
+                break;
+            case 44:
+                pokesound = p44;
+                break;
+            case 45:
+                pokesound = p45;
+                break;
+            case 46:
+                pokesound = p46;
+                break;
+            case 47:
+                pokesound = p47;
+                break;
+            case 48:
+                pokesound = p48;
+                break;
+            case 49:
+                pokesound = p49;
+                break;
+            case 50:
+                pokesound = p50;
+                break;
+            case 51:
+                pokesound = p51;
+                break;
+            case 52:
+                pokesound = p52;
+                break;
+            case 53:
+                pokesound = p53;
+                break;
+            case 54:
+                pokesound = p54;
+                break;
+            case 55:
+                pokesound = p55;
+                break;
+            case 56:
+                pokesound = p56;
+                break;
+            case 57:
+                pokesound = p57;
+                break;
+            case 58:
+                pokesound = p58;
+                break;
+            case 59:
+                pokesound = p59;
+                break;
+            case 60:
+                pokesound = p60;
+                break;
+            case 61:
+                pokesound = p61;
+                break;
+            case 62:
+                pokesound = p62;
+                break;
+            case 63:
+                pokesound = p63;
+                break;
+            case 64:
+                pokesound = p64;
+                break;
+            case 65:
+                pokesound = p65;
+                break;
+            case 66:
+                pokesound = p66;
+                break;
+            case 67:
+                pokesound = p67;
+                break;
+            case 68:
+                pokesound = p68;
+                break;
+            case 69:
+                pokesound = p69;
+                break;
+            case 70:
+                pokesound = p70;
+                break;
+            case 71:
+                pokesound = p71;
+                break;
+            case 72:
+                pokesound = p72;
+                break;
+            case 73:
+                pokesound = p73;
+                break;
+            case 74:
+                pokesound = p74;
+                break;
+            case 75:
+                pokesound = p75;
+                break;
+            case 76:
+                pokesound = p76;
+                break;
+            case 77:
+                pokesound = p77;
+                break;
+            case 78:
+                pokesound = p78;
+                break;
+            case 79:
+                pokesound = p79;
+                break;
+            case 80:
+                pokesound = p80;
+                break;
+            case 81:
+                pokesound = p81;
+                break;
+            case 82:
+                pokesound = p82;
+                break;
+            case 83:
+                pokesound = p83;
+                break;
+            case 84:
+                pokesound = p84;
+                break;
+            case 85:
+                pokesound = p85;
+                break;
+            case 86:
+                pokesound = p86;
+                break;
+            case 87:
+                pokesound = p87;
+                break;
+            case 88:
+                pokesound = p88;
+                break;
+            case 89:
+                pokesound = p89;
+                break;
+            case 90:
+                pokesound = p90;
+                break;
+            case 91:
+                pokesound = p91;
+                break;
+            case 92:
+                pokesound = p92;
+                break;
+            case 93:
+                pokesound = p93;
+                break;
+            case 94:
+                pokesound = p94;
+                break;
+            case 95:
+                pokesound = p95;
+                break;
+            case 96:
+                pokesound = p96;
+                break;
+            case 97:
+                pokesound = p97;
+                break;
+            case 98:
+                pokesound = p98;
+                break;
+            case 99:
+                pokesound = p99;
+                break;
+            case 100:
+                pokesound = p100;
+                break;
+            case 101:
+                pokesound = p101;
+                break;
+            case 102:
+                pokesound = p102;
+                break;
+            case 103:
+                pokesound = p103;
+                break;
+            case 104:
+                pokesound = p104;
+                break;
+            case 105:
+                pokesound = p105;
+                break;
+            case 106:
+                pokesound = p106;
+                break;
+            case 107:
+                pokesound = p107;
+                break;
+            case 108:
+                pokesound = p108;
+                break;
+            case 109:
+                pokesound = p109;
+                break;
+            case 110:
+                pokesound = p110;
+                break;
+            case 111:
+                pokesound = p111;
+                break;
+            case 112:
+                pokesound = p112;
+                break;
+            case 113:
+                pokesound = p113;
+                break;
+            case 114:
+                pokesound = p114;
+                break;
+            case 115:
+                pokesound = p115;
+                break;
+            case 116:
+                pokesound = p116;
+                break;
+            case 117:
+                pokesound = p117;
+                break;
+            case 118:
+                pokesound = p118;
+                break;
+            case 119:
+                pokesound = p119;
+                break;
+            case 120:
+                pokesound = p120;
+                break;
+            case 121:
+                pokesound = p121;
+                break;
+            case 122:
+                pokesound = p122;
+                break;
+            case 123:
+                pokesound = p123;
+                break;
+            case 124:
+                pokesound = p124;
+                break;
+            case 125:
+                pokesound = p125;
+                break;
+            case 126:
+                pokesound = p126;
+                break;
+            case 127:
+                pokesound = p127;
+                break;
+            case 128:
+                pokesound = p128;
+                break;
+            case 129:
+                pokesound = p129;
+                break;
+            case 130:
+                pokesound = p130;
+                break;
+            case 131:
+                pokesound = p131;
+                break;
+            case 132:
+                pokesound = p132;
+                break;
+            case 133:
+                pokesound = p133;
+                break;
+            case 134:
+                pokesound = p134;
+                break;
+            case 135:
+                pokesound = p135;
+                break;
+            case 136:
+                pokesound = p136;
+                break;
+            case 137:
+                pokesound = p137;
+                break;
+            case 138:
+                pokesound = p138;
+                break;
+            case 139:
+                pokesound = p139;
+                break;
+            case 140:
+                pokesound = p140;
+                break;
+            case 141:
+                pokesound = p141;
+                break;
+            case 142:
+                pokesound = p142;
+                break;
+            case 143:
+                pokesound = p143;
+                break;
+            case 144:
+                pokesound = p144;
+                break;
+            case 145:
+                pokesound = p145;
+                break;
+            case 146:
+                pokesound = p146;
+                break;
+            case 147:
+                pokesound = p147;
+                break;
+            case 148:
+                pokesound = p148;
+                break;
+            case 149:
+                pokesound = p149;
+                break;
+            case 150:
+                pokesound = p150;
+                break;
+            case 151:
+                pokesound = p151;
+                break;
+        }
+        var audio = new Audio(pokesound)
+        audio.play();
     }
 
     // BUTTONS FUNCTIONS
@@ -146,7 +604,7 @@ function GlobalState(props) {
                     playPcOn,
                     playPcOff,
                     playRun,
-                    playAPress,
+                    playPokeSound,
                     detailButton,
                     setDetailButton,
                     addPokedex,
